a9e68901617e26d3f2af74375de0da21018f6b6c
ManteshKalyankar <kalyankarmantesh@gmail.com>
2025-09-17 19:31:54 +0530

    Update: made changes to the project

--- src/Components/StopWatch/StopWatch.jsx
+++ src/Components/StopWatch/StopWatch.jsx
@@ -33,13 +33,12 @@ export default function StopWatch() {
     let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
     let seconds = Math.floor((elapsedTime / 1000) % 60);
-    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
 
     hours = String(hours).padStart(2, "0");
     minutes = String(minutes).padStart(2, "0");
     seconds = String(seconds).padStart(2, "0");
-    milliseconds = String(milliseconds).padStart(2, "0");
-    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+    return `${hours}:${minutes}:${seconds}`;
   }
   return (
     <div className="stopwatch">
